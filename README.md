bdf0a78e91e10c7bc7ba29c80a936db81a6da3a3
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2024-05-29 00:03:22 +0300

    ee

--- README.md
+++ README.md
@@ -36,7 +36,7 @@
 
 This project is licensed under the MPL 2.0 License.
 
-[![License](https://img.shields.io/badge/License-undefined-brightgreen.svg)](https://opensource.org/licenses/mpl-2-0)
+[![License](https://img.shields.io/badge/License-mpl2.0-brightgreen.svg)](https://opensource.org/licenses/mpl-2-0)
 View the [license](https://opensource.org/licenses/mpl-2-0) for more information.
 
 
@@ -44,4 +44,4 @@ MPL 2.0
 
 ## Questions
 
-For any questions, please reach out to me via [GitHub](https://github.com/) or [email](mailto:).
+For any questions, please reach out to me via [GitHub](https://github.com/e) or [email](mailto:).
--- index.js
+++ index.js
@@ -106,6 +106,7 @@ function renderLicenseBadge(license) {
   if (license === "None") return ""; // Возвращаем пустую строку, если лицензия не выбрана
 
   let licenseURL_IMG;
+
   switch (license) {
     case "BSD 2-Clause":
       licenseURL_IMG = "BSD2Clause";
@@ -116,16 +117,20 @@ function renderLicenseBadge(license) {
       licenseURL = "bsd-3-clause";
       break;
     case "Apache 2.0":
+      licenseURL_IMG = "apache2.0";
       licenseURL = "apache-2-0";
       break;
     case "GPLv3":
       licenseURL = "gpl-3-0";
+      licenseURL_IMG = "GPLv3";
       break;
     case "MPL 2.0":
       licenseURL = "mpl-2-0";
+      licenseURL_IMG = "mpl2.0";
       break;
     case "MIT":
       licenseURL = "mit";
+      licenseURL_IMG = "mit";
       break;
     default:
       licenseURL_IMG = license.replace(/\s/g, "%20");
